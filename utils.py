6789a900b6a45dcc39c27ddb1b99fd0d9d83b88b
Emma De Charry <emmadecharry@MacBook-Pro-de-Emma.local>
2025-03-11 14:58:12 +0100

    create utils

---
+++ utils.py
@@ -0,0 +1,62 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import networkx as nx
+from tqdm import tqdm
+import time
+
+def euclidean_kernel(x, y):
+    """
+    Compute the Euclidean kernel between two vectors.
+    """
+    return np.exp(-np.linalg.norm(x - y)**2)
+
+def compute_kernel_matrix(X, Y):
+    """
+    Compute the kernel matrix between two sets of vectors.
+    """
+    rows, cols = X.shape[0], Y.shape[0]
+    K = np.zeros((rows, cols))
+    
+    for i in range(rows):
+        for j in range(cols):
+            K[i, j] = euclidean_kernel(X[i], Y[j])
+    
+    return K
+
+def compute_alpha_star(Kmm, Knm, y, sigma2, nu):
+    """
+    Compute the alpha_star vector.
+    """
+    m = Kmm.shape[0]
+    A = sigma2 * Kmm + Knm.T @ Knm + nu * np.eye(m)
+    b = Knm.T @ y
+    
+    return np.linalg.solve(A, b)
+
+def local_objective_function(alpha, sigma2, K_mm, y_loc, K_im, nu, a):
+    """
+    Compute the local objective function value.
+    """
+    t1 = (sigma2 / (2 * a)) * alpha.T @ K_mm @ alpha
+    t2 = 0.5 * np.sum((y_loc - K_im @ alpha) ** 2)
+    t3 = (nu / (2 * a)) * np.linalg.norm(alpha) ** 2
+    
+    return t1 + t2 + t3
+
+def compute_local_gradient(alpha, sigma2, K_mm, y_loc, K_im, nu, a):
+    """
+    Compute the local gradient.
+    """
+    grad = (sigma2 / a) * (K_mm @ alpha)
+    grad += K_im.T @ (K_im @ alpha - y_loc)
+    grad += (nu / a) * alpha
+    
+    return grad
+
+def nystrom_approx(alpha, X_selected, X):
+    """
+    Compute the Nystrom approximation.
+    """
+    K1m = compute_kernel_matrix(X, X_selected)
+    return K1m @ alpha
