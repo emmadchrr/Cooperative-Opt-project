3b907ea5dbacd5a53921a905f558bd5622b06a05
Emma De Charry <emmadecharry@macbookpro.home>
2025-03-19 12:29:15 +0100

    dual dec et dgd un peu mieux mais pas ouf

--- dgd.py
+++ dgd.py
@@ -1,4 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import networkx as nx
 from utils import *
 
 
@@ -16,24 +19,74 @@ def DGD(X, Y, X_selected, a, nu, sigma2, n_epochs, alpha_star, W, step_size):
     opt_gaps = [[] for _ in range(a)]
     alphas = []
 
-    for epoch in range(n_epochs):
+    for _ in range(n_epochs):
         alphas.append(alpha)
-        gradients = np.zeros((m*a, 1))
+        gradients = np.zeros((m * a, 1))
 
-        for a_idx, data_idx in enumerate(a_data_idx):
-            X_loc = X[data_idx]
-            y_loc = Y[data_idx].reshape(-1, 1)
-            Kim = compute_kernel_matrix(X_loc, X_selected)
-            local_gradient = compute_local_gradient(alpha[m * a_idx: m * (a_idx + 1)], sigma2, Kmm, y_loc, Kim, nu, a)
-            gradients[m * a_idx: m * (a_idx + 1)] = local_gradient
+        for agent_idx, data_idx in enumerate(a_data_idx):
+            X_local = X[data_idx]
+            y_local = Y[data_idx].reshape(-1, 1)
+            K_im = compute_kernel_matrix(X_local, X_selected)
+            local_gradient = compute_local_gradient(alpha[m * agent_idx: m * (agent_idx + 1)], sigma2, Kmm, y_local, K_im, nu, a)
+            gradients[m * agent_idx: m * (agent_idx + 1)] = local_gradient
         
         alpha = W @ alpha - step_size * gradients
         
-        for a_idx in range(a):
-            alpha_agent = alpha[m * a_idx: m * (a_idx + 1)].reshape(-1, 1)
-            opt_gap = np.linalg.norm(alpha_agent - alpha_star.reshape(-1, 1))
-            opt_gaps[a_idx].append(opt_gap)
-        
-     
+        for agent_idx in range(a):
+            alpha_agent = alpha[m * agent_idx: m * (agent_idx + 1)].reshape(-1, 1)
+            optimality_gap = np.linalg.norm(alpha_agent - alpha_star.reshape(-1, 1))
+            opt_gaps[agent_idx].append(optimality_gap)
         
     return opt_gaps, alphas
+
+
+if __name__ == "__main__":
+    with open('first_database.pkl', 'rb') as f:
+        x, y = pickle.load(f)
+    
+    n, m, a = 100, 10, 5
+    sigma2 = 0.25
+    nu = 1
+    beta = 10
+    n_epochs = 100
+    sigma = 0.5
+    step_size = 0.002
+
+    # Generate data
+    x_n = x[:n] 
+    y_n = y[:n]
+
+    sel = [i for i in range(n)]
+    ind = np.random.choice(sel, m, replace=False)
+    x_selected = np.array([x[i] for i in ind])
+    Kmm = compute_kernel_matrix(x_selected, x_selected)
+    Knm = compute_kernel_matrix(x_n, x_selected)
+    alpha_star = compute_alpha_star(Kmm, Knm, y_n, sigma2, nu)
+    W = np.zeros((a,a))
+    K = compute_kernel_matrix(x_n, x_n)
+    selected_pts_agents = np.array_split(np.random.permutation(n), a)
+    lr = 0.01
+
+    start = time.time()
+    alpha_optimal = compute_alpha_star(Kmm, Knm, y_n, sigma2, nu)
+    end = time.time()
+    print(f'Time to compute alpha optimal : {end - start}\n')
+    print(f'Optimal alpha : {alpha_optimal}\n')
+
+    start = time.time()
+    opt_gaps, alphas = DGD(
+        x_n, y_n, x_selected, a, nu, sigma2, n_epochs, alpha_optimal, W, 0.01)
+    end = time.time()
+    dgd_time = end - start
+    
+    for agent_idx, optimality_gaps in enumerate(opt_gaps):
+        plt.plot(range(n_epochs), optimality_gaps, label=f"Agent {agent_idx + 1}")
+
+    plt.xlabel('Number of iterations')
+    plt.ylabel('Optimality Gap')
+    plt.title(f'DGD Convergence, Step size : {step_size}')
+    plt.legend()
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.grid(True)
+    plt.show()
--- dual_decomposition.py
+++ dual_decomposition.py
@@ -7,10 +7,10 @@ def solve_alpha_dualdec(x, y, selected_points, selected_points_agent, sigma, nu,
     n = len(x)
     a = len(selected_points_agent)
     m = len(selected_points)
-    Kmm = compute_kernel_matrix(x[selected_points], x[selected_points])
+    Kmm = compute_kernel_matrix(selected_points, selected_points)
     alpha = []
     for i in range(a):
-        Kim = compute_kernel_matrix(x[selected_points_agent[i]], x[selected_points])
+        Kim = compute_kernel_matrix(x[selected_points_agent[i]], selected_points)
         A = sigma**2 * Kmm + np.eye(m)*nu + np.transpose(Kim) @ Kim
         b = np.transpose(Kim) @ y[selected_points_agent[i]]
         for j in range(a):
@@ -36,7 +36,7 @@ def dualDec(x, y, selected_points, selected_points_agent, K, sigma, nu, lr, W, m
         alpha_optim = np.zeros((a,m))
         alpha_optim = solve_alpha_dualdec(
             x, y, selected_points, selected_points_agent, sigma, nu,
-            K, graph, lambda_ij)
+            graph, lambda_ij)
         for i in range(a):
             for j in range(i):
                 lambda_ij[i, j, : ] += lr * (alpha_optim[i, :] - alpha_optim[j, :])
@@ -47,3 +47,84 @@ def dualDec(x, y, selected_points, selected_points_agent, K, sigma, nu, lr, W, m
     alpha_optim = np.mean(alpha_optim, axis=0)
 
     return alpha_optim, alpha_list_agent, alpha_mean_list
+
+if __name__ == "__main__":
+    with open('first_database.pkl', 'rb') as f:
+        x, y = pickle.load(f)
+    
+    n, m, a = 100, 10, 5
+    sigma2 = 0.25
+    nu = 1
+    beta = 10
+    n_epochs = 100
+    sigma = 0.5
+
+    # Generate data
+    x_n = x[:n] 
+    y_n = y[:n]
+
+    sel = [i for i in range(n)]
+    ind = np.random.choice(sel, m, replace=False)
+    x_selected = np.array([x[i] for i in ind])
+    Kmm = compute_kernel_matrix(x_selected, x_selected)
+    Knm = compute_kernel_matrix(x_n, x_selected)
+    alpha_star = compute_alpha_star(Kmm, Knm, y_n, sigma2, nu)
+    W = np.ones((a,a))
+    K = compute_kernel_matrix(x_n, x_n)
+    selected_pts_agents = np.array_split(np.random.permutation(n), a)
+    lr = 0.01
+
+    start = time.time()
+    alpha_optimal = compute_alpha_star(Kmm, Knm, y_n, sigma2, nu)
+    end = time.time()
+    print(f'Time to compute alpha optimal : {end - start}\n')
+    print(f'Optimal alpha : {alpha_optimal}\n')
+    start = time.time()
+    alpha_optim, alpha_list, alpha_mean_list = dualDec(
+        x_n, y_n, x_selected, selected_pts_agents,
+        K, sigma, nu, 0.01, W, max_iter=1000, lamb0=0.
+    )
+    end = time.time()
+    print(f'alpha optimal with dual decomposition : {alpha_optim}')
+    print(
+        f'Time to compute alpha optimal with dual decomposition : {end - start}')
+    # print(f'Total iterations : {tot_ite}\n')
+
+    # Data visualization
+    Y = np.linalg.norm(alpha_list - alpha_optim, axis=1)
+    # unpack the list of alpha to get for each agent the evolution of alpha
+    agent_1 = np.linalg.norm(np.array(
+        [alpha_list[i][0] for i in range(len(alpha_list))]) - alpha_optim, axis=1)
+    agent_2 = np.linalg.norm(np.array(
+        [alpha_list[i][1] for i in range(len(alpha_list))]) - alpha_optim, axis=1)
+    agent_3 = np.linalg.norm(np.array(
+        [alpha_list[i][2] for i in range(len(alpha_list))]) - alpha_optim, axis=1)
+    agent_4 = np.linalg.norm(np.array(
+        [alpha_list[i][3] for i in range(len(alpha_list))]) - alpha_optim, axis=1)
+    agent_5 = np.linalg.norm(np.array(
+        [alpha_list[i][4] for i in range(len(alpha_list))]) - alpha_optim, axis=1)
+
+    plt.plot(agent_1, label='Agent 1', color='blue')
+    plt.plot(agent_2, label='Agent 2', color='red')
+    plt.plot(agent_3, label='Agent 3', color='green')
+    plt.plot(agent_4, label='Agent 4', color='orange')
+    plt.plot(agent_5, label='Agent 5', color='purple')
+    plt.xlabel('Iterations')
+    plt.ylabel('Optimality gap (norm)')
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.grid()
+    plt.show()
+    # Plot selected points and the prediction of the model with the alpha optimal 
+    plt.figure(0)
+    for i in range(a):
+        plt.plot(x_n[i], y_n[i], 'o', label=f'Agent {i+1}')
+    # plt.plot(x[0:n], y[0:n], 'o', label='Data')
+    x_predict = np.linspace(-1, 1, 250)
+    K_f = compute_kernel_matrix(x_predict, x_selected)
+    # fx_predict = get_Kij(range(n), selected_points, K) @ alpha_optim_gt
+    fx_predict = K_f @ alpha_optim
+    plt.plot(x_predict, fx_predict, label='Prediction')
+    plt.grid()
+    plt.legend()
+    plt.show()
