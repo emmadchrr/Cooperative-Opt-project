c3521442029f48d8f073635aa4008211c66eea39
Emma De Charry <emmadecharry@macbookpro.home>
2025-03-24 17:51:21 +0100

    r

--- methods/dgd.py
+++ methods/dgd.py
@@ -4,8 +4,7 @@ import pickle
 import networkx as nx
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'utils')))
-from utils import compute_kernel_matrix, compute_alpha_star, grad_alpha
+from utils import *
 import time
 
 def DGD(X, Y, X_selected, a, nu, sigma2, alpha_star, W, step_size, n_epochs=500):
@@ -61,8 +60,8 @@ if __name__ == "__main__":
     Kmm = compute_kernel_matrix(x_selected, x_selected)
     Knm = compute_kernel_matrix(x_n, x_selected)
     alpha_star = compute_alpha_star(Kmm, Knm, y_n, sigma2, nu)
-    W = np.ones((a, a))
-    #W = W(a)
+    #W = np.ones((a, a))
+    W = W(a)
     K = compute_kernel_matrix(x_n, x_n)
     selected_pts_agents = np.array_split(np.random.permutation(n), a)
     step_size = 0.0009
